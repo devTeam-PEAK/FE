9d9468d7915004f98e00507c9a6778983207770b
배근영 <bgy09270@naver.com>
2026-04-23 20:02:03 +0900

    design: 마이페이지 아이콘 커스텀 (#6)

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,8 +1,12 @@
+import CircleUserRound from "@/components/ui/circle-user-round";
+
 export default function Header() {
   return (
     <div className="sticky top-0 z-999 flex h-14 w-full items-center justify-between bg-white">
       <div className="h3-bold text-main">Peak</div>
-      <div>마이페이지</div>
+      <div>
+        <CircleUserRound size={24} />
+      </div>
     </div>
   );
 }

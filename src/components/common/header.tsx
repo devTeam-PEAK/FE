a186ddbd7773383de4cf83c4d530963d0227af7f
배근영 <bgy09270@naver.com>
2026-04-27 15:00:26 +0900

    feat: 로그인 상태에 따라 헤더 아이콘 노출 여부 조건부 처리 (#23)

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,16 +1,22 @@
 import CircleUserRound from "@/components/ui/circle-user-round";
 import Image from "next/image";
 import Link from "next/link";
+import { cookies } from "next/headers";
 
-export default function Header() {
+export default async function Header() {
+  const cookieStore = await cookies();
+  const isLoggedIn = cookieStore.has("accessToken");
+
   return (
     <div className="sticky top-0 z-999 flex h-14 w-full items-center justify-between bg-white">
       <div className="h3-bold text-main">
         <Image src={"/logo.svg"} alt="Logo" width={56} height={14} />
       </div>
-      <Link href="/mypage">
-        <CircleUserRound size={24} />
-      </Link>
+      {isLoggedIn && (
+        <Link href="/mypage">
+          <CircleUserRound size={24} />
+        </Link>
+      )}
     </div>
   );
 }

cdd008c3a5ca3c4063f48ad5709005de079ab73b
uzzin <01uzzin@gmail.com>
2026-04-28 17:09:19 +0900

    feat: 앨범 상세 페이지 퍼블리싱

--- src/components/album/streaming-button.tsx
+++ src/components/album/streaming-button.tsx
@@ -10,7 +10,7 @@ export default function StreamingButton({ streamingType }: Props) {
   const { label, icon } = mapToStreamingBtn[streamingType];
 
   return (
-    <button className="bg-grey1 flex h-14 w-42 items-center gap-3 rounded-2xl px-4">
+    <button className="bg-grey1 flex h-14 w-full items-center gap-3 rounded-2xl px-4">
       <Image src={icon} alt={label} width={32} height={32} />
       <span className="p2-bold text-font-middle">{label}</span>
     </button>

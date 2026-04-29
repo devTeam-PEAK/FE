6940bd462415e24eb832485624bd8a746f6f024a
배근영 <bgy09270@naver.com>
2026-04-29 15:20:25 +0900

    design: 메시지 줄바꿈 수정 (#48)

--- src/components/login/login-error-toast.tsx
+++ src/components/login/login-error-toast.tsx
@@ -9,9 +9,19 @@ interface Props {
 
 export default function LoginErrorToast({ withdrawnAt }: Props) {
   useEffect(() => {
-    const message = withdrawnAt
-      ? `${withdrawnAt}에 탈퇴처리된 계정입니다. \n90일 이후 재가입 가능합니다.`
-      : "탈퇴처리된 계정입니다. 90일 이후 재가입 가능합니다.";
+    const message = withdrawnAt ? (
+      <span>
+        {withdrawnAt}에 탈퇴처리된 계정입니다.
+        <br />
+        90일 이후 재가입 가능합니다.
+      </span>
+    ) : (
+      <span>
+        탈퇴처리된 계정입니다.
+        <br />
+        90일 이후 재가입 가능합니다.
+      </span>
+    );
     toast.error(message, { duration: 3000 });
   }, [withdrawnAt]);
 

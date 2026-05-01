c16bc0a9c818454731458039634a41475a190e44
uzzin <01uzzin@gmail.com>
2026-05-01 14:04:42 +0900

    refactor: 스트리밍 아이콘 해상도 개선을 위해 SVG로 교체

--- src/utils/mapper.ts
+++ src/utils/mapper.ts
@@ -9,22 +9,22 @@ export const mapToStreamingBtn: Record<
 > = {
   spotify: {
     label: "스포티파이",
-    icon: "/spotify.png",
+    icon: "/spotify.svg",
   },
   applemusic: {
     label: "애플 뮤직",
-    icon: "/apple-music.png",
+    icon: "/apple-music.svg",
   },
   melon: {
     label: "멜론",
-    icon: "/melon.png",
+    icon: "/melon.svg",
   },
   youtubemusic: {
     label: "유튜브 뮤직",
-    icon: "/youtube-music.png",
+    icon: "/youtube-music.svg",
   },
   soundcloud: {
     label: "사운드클라우드",
-    icon: "/soundcloud.png",
+    icon: "/soundcloud.svg",
   },
 };

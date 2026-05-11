2282b7f40a40dfddc06c107250d8112036175c1b
배근영 <bgy09270@naver.com>
2026-05-11 15:03:22 +0900

    design: 이미지 정리 및 변경사항 페이지반영(#102)

--- src/components/home/home-buttons.tsx
+++ src/components/home/home-buttons.tsx
@@ -26,7 +26,7 @@ export default function HomeButtons({ showIntro }: { showIntro: boolean }) {
           onClick={dismissIntro}
         />
       )}
-      <div className="flex flex-col gap-4">
+      <div className="flex w-full flex-col gap-2">
         <Tooltip open={showIntroState}>
           <TooltipTrigger asChild>
             <Button
@@ -34,7 +34,7 @@ export default function HomeButtons({ showIntro }: { showIntro: boolean }) {
               size="full"
               onClick={() => router.push("/album")}
             >
-              신곡 홍보 링크 만들기
+              앨범 홍보 링크 만들기
             </Button>
           </TooltipTrigger>
           <TooltipContent>

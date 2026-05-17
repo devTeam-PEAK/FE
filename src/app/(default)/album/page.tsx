1d49e84d9cf5caa4d45bd448983b06ac843f98a0
uzzin <01uzzin@gmail.com>
2026-05-17 16:09:09 +0900

    refactor: 클라이언트 컴포넌트 폴더 경로 수정

--- src/app/(default)/album/page.tsx
+++ src/app/(default)/album/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import AlbumPage from "./_client-album-page";
+import AlbumPage from "@/components/album/album-page";
 
 export default function Page() {
   return (
